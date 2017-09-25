be6b143a5d8eab6de5c07855dc72dd47e106c62c
Razvan Mihalcea <rtmihalcea@gmail.com>
2017-09-25 12:34:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
 function add(a, b) {
-  return a + b
+  return a + b;
 }
 
 function subtract(a, b) {
-  return a - b
+  return a - b;
 }
 
 function multiply(a, b) {
-  return a * b
+  return a * b;
 }
 
 function divide(a, b) {
-  return a / b
+  return a / b;
 }
 
+var a = 0
 function inc(n) {
-  n = a + 1
-  return n++
+  n++
+  return n;
 }
+a=inc(a)
